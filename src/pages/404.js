fac6fc0f49544b8832f928eeb2c7457e08afc989
Dalina Weidinger <dalina.weidinger@gmail.com>
2020-09-28 12:14:09 +0200

    not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,34 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import ParticlesLanding from "../components/ParticlesLanding"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ParticlesLanding />
+
+    <NotFound className="flex-col align-center justify-center ">
+      <div>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist...</p>
+      </div>
+    </NotFound>
   </Layout>
 )
 
 export default NotFoundPage
+const NotFound = styled.div`
+  height: 100vh;
+  background: var(--white);
+  position: absolute;
+  left: 50%;
+  right: 50%;
+  top: 0;
+
+  div {
+    width: 300px;
+    text-align: center;
+  }
+`
